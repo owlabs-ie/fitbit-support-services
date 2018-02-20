3088f399b9cf0602d57dbb07ffa805a9dca0c76e
Tulio Castro <tcastro@fexco.com>
2018-02-20 16:43:43 +0000

    spring cloud config

---
+++ Jenkinsfile
@@ -0,0 +1,37 @@
+node {
+
+  def buildError = false
+
+  //Update the project first
+  stage('Checkout') {
+      echo "Checking out project..."
+      checkout scm
+  }
+
+  try{
+
+    //Build Project
+    stage('Build') {
+        sh "./mvnw clean install"
+    }
+
+    switch (env.BRANCH_NAME){
+
+      case "master":
+
+        stage('Updating and running') {
+            sh "docker-compose up -d"
+        }
+
+        break;
+
+    }
+
+  } catch (Exception ex) {
+    buildError = true;
+    throw ex
+  } finally {
+    //TODO Slack msgs
+  }
+
+}
---
+++ src/test/java/es/flaviojmend/fitbittracker/TestMainApplication.java
@@ -0,0 +1,34 @@
+package es.flaviojmend.fitbittracker;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {MongoDataAutoConfiguration.class})
+public class TestMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMainApplication.class, args);
+    }
+
+}
