1ff05c1bc3139730f9ad1da9ade76a7da00e473c
Tulio Castro <tcastro@fexco.com>
2018-03-01 10:30:35 +0000

    removed redirection server

--- src/main/java/es/flaviojmend/fitbittracker/AppConfiguration.java
+++ src/main/java/es/flaviojmend/fitbittracker/AppConfiguration.java
@@ -21,34 +21,34 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableCaching
 @EnableAsync
 public class AppConfiguration {
-
-    @Bean
-    @ConditionalOnProperty("server.ssl.enabled")
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-
-        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-        return tomcat;
-    }
-
-
-     private Connector initiateHttpConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("https");
-        connector.setSecure(true);
-        connector.setPort(8443);
-
-        return connector;
-    }
+//
+//    @Bean
+//    @ConditionalOnProperty("server.ssl.enabled")
+//    public EmbeddedServletContainerFactory servletContainer() {
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//
+//        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+//        return tomcat;
+//    }
+//
+//
+//     private Connector initiateHttpConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("https");
+//        connector.setSecure(true);
+//        connector.setPort(8443);
+//
+//        return connector;
+//    }
 
 }
